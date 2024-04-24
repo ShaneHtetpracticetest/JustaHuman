e51a04a08e1125b63c17cdf40f0dbd88b124722b
ShaneHtetpracticetest <shanehtetaung2236@gmail.com>
2024-04-24 12:24:05 +0700

    24_4_2024

--- test15.py
+++ test15.py
@@ -1,7 +1,20 @@
-for x in range(1,6):
-    print("I Count from " , x)
+# LOOP BY USING FOR AND RANGE
 
-counts = [1,2,3,4,5]
-for x in counts:
+# for x in range(1,6):
+#     print("I Count from " , x)
+
+# counts = [1,2,3,4,5]
+# for x in counts:
+#     print("I Count from ", x)
+
+
+
+# LOOP BY USING WHILE
+
+x = 1
+while x < 7:
     print("I Count from ", x)
-    
+    x += 1
+ 
+
+
