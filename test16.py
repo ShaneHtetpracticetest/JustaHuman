b9d9046c64203b15c5a61fa0751e2c48dcd7062a
ShaneHtetpracticetest <shanehtetaung2236@gmail.com>
2024-04-29 11:32:31 +0700

    29_4_2024

--- test16.py
+++ test16.py
@@ -1,3 +1,8 @@
 Collection = ['Pepsi', 'Olivia', 'Selena', 'Sza', 'Lana', 'Yurisa']
 print(Collection)
 print(type(Collection))
+
+print(Collection[0])
+print(Collection[0:5])
+print(Collection[:5])
+print(Collection[0:])
