f8f704237d17117e182c8946f0280c610c26160e
ShaneHtetpracticetest <shanehtetaung2236@gmail.com>
2024-04-29 12:16:17 +0700

    29_4_2024

---
+++ test17.py
@@ -0,0 +1,11 @@
+# list []
+Collection = ['Pepsi', 'Da hell', 'B']
+print(type(Collection))
+
+# tuple()
+Collection = ('Pepsi', 'Da hell', 'B')
+print(type(Collection))
+
+# set{}
+Collection = {'Pepsi', 'Da hell', 'B'}
+print(type(Collection))
--- test18.py
+++ test18.py
@@ -3,4 +3,11 @@ fav_anime = {
     'genre': 'Fantasy',
     'Season': 1
 }
-print(fav_anime['name'])
+# print(fav_anime['name'])
+# fav_anime['genre'] = 'Drama'
+# print(fav_anime['genre'])
+
+fav_anime["favorite_episode"] = 1
+print(fav_anime["favorite_episode"])
+
+
