413925fba83df80df40fa61ebeb27d8189d0b9ba
ShaneHtetpracticetest <shanehtetaung2236@gmail.com>
2024-04-30 12:25:51 +0700

    30_4_2024

---
+++ test23.py
@@ -0,0 +1,5 @@
+def user_info(name, mail, addr="Yangon"):
+    print("Your name is ", name)
+    print("Your mail is ", mail)
+    print("Your addr is ", addr)
+
---
+++ test24.py
@@ -0,0 +1,10 @@
+from test23 import *
+
+# positional
+user_info("sha", "sha@gmail.com", "Yangon")
+
+# keyword
+user_info(addr="Yangon", mail="sha@gmail.com", name="sha")
+
+#default
+user_info("sha", "sha@gmail.com")
