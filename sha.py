ce9b42d13f559cbbacdc6577c64d3c2e2d7d8ce0
ShaneHtetpracticetest <shanehtetaung2236@gmail.com>
2024-04-30 09:39:43 +0700

    testing

--- sha.py
+++ sha.py
@@ -10,39 +10,55 @@
 #     print("Get the fk out of here and come back when you are 18")
 
 
-print("""                
+# print("""                
 
-        Welcome from SHA's calculation 
+#         Welcome from SHA's calculation 
 
-""")
-while True:
-    print("Enter the operation you want to perform (+, -, *, /) or 'quit' to exit:")
+# """)
+# while True:
+#     print("Enter the operation you want to perform (+, -, *, /) or 'quit' to exit:")
     
-    operation = input("Operation: ")
+#     operation = input("Operation: ")
 
-    if operation.lower() == 'quit':
-        print("Exiting the calculator.")
-        break
+#     if operation.lower() == 'quit':
+#         print("Exiting the calculator.")
+#         break
     
-    num1 = float(input("Enter first number: "))
-    num2 = float(input("Enter second number: "))
+#     num1 = float(input("Enter first number: "))
+#     num2 = float(input("Enter second number: "))
     
-    if operation == '+':
-        result = num1 + num2
-        print("Result:", result)
-    elif operation == '-':
-        result = num1 - num2
-        print("Result:", result)
-    elif operation == '*':
-        result = num1 * num2
-        print("Result:", result)
-    elif operation == '/':
-        if num2 == 0:
-            print("Error! Division by zero.")
-        else:
-            result = num1 / num2
-            print("Result:", result)
-    else:
-        print("Invalid operation! Please enter a valid operation (+, -, *, /).")
+#     if operation == '+':
+#         result = num1 + num2
+#         print("Result:", result)
+#     elif operation == '-':
+#         result = num1 - num2
+#         print("Result:", result)
+#     elif operation == '*':
+#         result = num1 * num2
+#         print("Result:", result)
+#     elif operation == '/':
+#         if num2 == 0:
+#             print("Error! Division by zero.")
+#         else:
+#             result = num1 / num2
+#             print("Result:", result)
+#     else:
+#         print("Invalid operation! Please enter a valid operation (+, -, *, /).")
+
+
+# my_list = [1,2,1,5,2,3,5,2,4]
+# print(my_list)
+
+# my_tuple = (1,2,3,5,2,3,4,5,2,)
+# print(my_tuple)
+
+# my_set = {1,3,4,2,1,4,5,2,4}
+# print(my_set)
+
+# list_without_duplicate = set(my_list)
+# print(list_without_duplicate)
+
+# tuple_without_duplicate = set(my_tuple)
+# print(tuple_without_duplicate)
 
 
--- test2.py
+++ test2.py
@@ -1,3 +1,3 @@
-print("Hello world")
+print("Hello")
 print("My age is " +  "18")
 print("My age is " +  str(18))
