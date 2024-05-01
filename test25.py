22c8537760e08b84bf81f0f9a91de0b769cc0e6e
ShaneHtetpracticetest <shanehtetaung2236@gmail.com>
2024-05-01 11:26:30 +0700

    1_5_2024

---
+++ test25.py
@@ -0,0 +1,4 @@
+# arbitary argument
+def marlarxiangou(*order):
+    print("Your order is :", order)
+    
---
+++ test26.py
@@ -0,0 +1,6 @@
+from test25 import marlarxiangou as marlar
+
+marlar('noodle', 'meat', 'vegetable')
+marlar('sea food', 'reduce pepper')
+marlar('meat', 'noodle', 'more chili')
+marlar('mushroom', 'meat')
