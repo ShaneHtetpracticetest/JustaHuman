744a4a7d8fde60324072d37ca38fbc5ee4ece5a9
ShaneHtetpracticetest <shanehtetaung2236@gmail.com>
2024-05-01 23:51:48 +0700

    freaking error and i give up for now

--- testing1.py
+++ testing1.py
@@ -1,9 +1,12 @@
+
 class Genshin():
 
-    def __init__(link, CharacterName, element, hp):
+    def __init__(link, CharacterName, element, hp, max_stamina):
         link.CharacterName = CharacterName
         link.element = element
         link.hp = hp
+        link.max_stamina = max_stamina
+        link.stamina = max_stamina
 
     def jump(link):
         print(link.CharacterName , " is jumping")
@@ -15,7 +18,36 @@ class Genshin():
 
     def sprint(link):
         print(link.CharacterName , " is sprinting")
+        
+        def decrease_stamina(link):
+            while True:
+                time.sleep(1)
+                if link.stamina > 0:
+                    link.stamina -= 30
+                else :
+                    link.stamina = 0
+
+        def sprinting(link):
+            if link.stamina >= sprint_cost: 
+                link.stamina -= sprint_cost
+                print(f"{link.name} is sprinting! Stamina: {link.stamina}")
+            else:
+                print(f"{link.name} doesn't have enough stamina to sprint! Stopping sprint...")
+
+    sprint_cost = 30
+
+    import threading
+    thread = threading.Thread(target=Cha2.decrease_stamina)
+    thread.daemon = True
+    thread.start()
+    
+while True:
+    Cha2.sprint()
+    if Cha2.stamina == 0:
+        break  
+    time.sleep(2)  
 
     def ElementalSkill(link):
         print(link.CharacterName , " is using elemental skill")
+
 
--- testing2.py
+++ testing2.py
@@ -1,9 +1,10 @@
 from testing1 import Genshin
 
-Cha1 = Genshin("Arlecchino" , "All is ash" , 30000)
+Cha1 = Genshin(CharacterName= "Arlecchino", element="Pyro", hp=30000, max_stamina=360)
 Cha1.ElementalSkill()
 Cha1.attack()
 
-Cha2 = Genshin("Nevulette" , "O tears I shall repay" , 40000)
-Cha2.attack()
+Cha2 = Genshin(CharacterName= "Nevuilette", element="Hydro", hp=40000, max_stamina=360)
+Cha2.sprint()
+
 
